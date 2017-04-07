169fcee021f033532f0a52ea0c557fa849677216
jcr <javier.crespo@ingenico.com>
2017-04-07 19:59:59 +0200

    Add project files.

---
+++ NSwagCustomSchema/Controllers/ValuesController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NJsonSchema;
+using NJsonSchema.Annotations;
+
+namespace NSwagCustomSchema.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+       
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public Foo Get(int id)
+        {
+            return new Foo();
+        }
+
+       
+    }
+
+    public class Foo
+    {
+        [JsonSchema(JsonObjectType.String)]
+        public Bar Bar { get; set; }
+    }
+
+    public class Bar
+    {
+        //TODO: Override Newtonsoft json serialization
+        public string Test { get; set; }
+    }
+}
